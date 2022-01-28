ee42ba4929b9cc744695684f053d231925fc8045
prangel-git <62568827+prangel-git@users.noreply.github.com>
2022-01-28 16:34:14 -0500

    First attempt to brute force search for legendre pairs.

--- legendre_pairs.py
+++ legendre_pairs.py
@@ -1,4 +1,9 @@
 from dft_utils import psd
+from sequence_generation import (
+    seq_module_rotation_and_reversal,
+    seq_binary,
+    seq_n_choose_k,
+)
 
 from vector_utils import circular_correlation, pointwise_operation
 
@@ -18,8 +23,57 @@ def are_compatible_psd(a, b):
     return all([are_almost_equal(result[1], result[i]) for i in range(1, len(result))])
 
 
+def brute_force_search_of_compatible_autocorrelations(n):
+    sequences_to_test = [
+        seq for seq in seq_module_rotation_and_reversal(seq_n_choose_k(n, n // 2))
+    ]
+
+    compatible_sequences = []
+    for seq_a in sequences_to_test:
+        for seq_b in sequences_to_test:
+            if are_compatible_autocorrelation(seq_a, seq_b):
+                compatible_sequence = (seq_a, seq_b)
+                compatible_sequences.append(compatible_sequence)
+
+    return compatible_sequences
+
+
+"""
+def brute_force_search_of_compatible_autocorrelations_1(n):
+
+    correlation_to_sequence = {
+        tuple(circular_correlation(seq, seq)): seq
+        for seq in seq_module_rotation_and_reversal(seq_n_choose_k(n, n // 2))
+    }
+
+    compatible_sequences = []
+    expected_addition = [n - 1] + [(n - 1) // 2] * (n - 1)
+    for correlation in correlation_to_sequence.keys():
+        expected_correlation = tuple(
+            pointwise_operation(lambda x, y: x - y, expected_addition, correlation)
+        )
+        if expected_correlation in correlation_to_sequence.keys():
+            compatible_sequence = (
+                list(correlation_to_sequence[correlation]),
+                list(correlation_to_sequence[expected_correlation]),
+            )
+            compatible_sequences.append(compatible_sequence)
+
+    return compatible_sequences
+"""
+
+
 def main():  # pragma: no cover
     print("Entry point for playing around")
+    n = 11
+    compatible_sequences = brute_force_search_of_compatible_autocorrelations(n)
+    for a, b in compatible_sequences:
+        correlation_a = circular_correlation(a, a)
+        correlation_b = circular_correlation(b, b)
+        correlation_sum = pointwise_operation(
+            lambda x, y: x + y, correlation_a, correlation_b
+        )
+        print(f"sequence a {a}, sequence b {b}, correlation sum {correlation_sum}")
 
 
 if __name__ == "__main__":  # pragma: no cover
--- test/test_legendre_pairs.py
+++ test/test_legendre_pairs.py
@@ -25,3 +25,12 @@ def test_not_are_compatible_psd():
     a = [1, 1, 1, -1, 1, -1, -1]
     b = [0, 1, 0, 1, 0, 1, 1]
     assert not are_compatible_psd(a, b)
+
+
+def test_brute_force_search_of_compatible_autocorrelations():
+    n = 11
+    compatible_autocorrelation = brute_force_search_of_compatible_autocorrelations(n)
+    assert compatible_autocorrelation
+
+    for seq_a, seq_b in compatible_autocorrelation:
+        assert are_compatible_psd(seq_a, seq_b)
