d8bc2eee5e76419092e435f6ff813578a5c86db1
prangel-git <62568827+prangel-git@users.noreply.github.com>
2022-01-31 11:01:41 -0500

    successor function for finding prenecklaces

--- necklaces_generation.py
+++ necklaces_generation.py
@@ -12,3 +12,34 @@ def find_necklace(sequence):
 
 def equal_necklaces(sequence_a, sequence_b):
     return find_necklace(sequence_a) == find_necklace(sequence_b)
+
+
+def successor(sequence):
+    len_sequence = len(sequence)
+    largest_index_equal_to_zero = find_largest_index_equal_to_zero(sequence)
+
+    if largest_index_equal_to_zero is None:
+        return None
+
+    number_of_repetitions = len_sequence // (largest_index_equal_to_zero + 1)
+    length_last_part = len_sequence % (largest_index_equal_to_zero + 1)
+
+    return (
+        sequence[:largest_index_equal_to_zero] + [1]
+    ) * number_of_repetitions + sequence[:length_last_part]
+
+
+def find_largest_index_equal_to_zero(sequence):
+    for k in range(len(sequence) - 1, -1, -1):
+        if sequence[k] == 0:
+            return k
+
+    return None
+
+
+def main():  # pragma: no cover
+    print("Entry point for playing around")
+
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
--- test/test_necklaces_generation.py
+++ test/test_necklaces_generation.py
@@ -1,3 +1,5 @@
+import pytest
+
 from context import necklaces_generation
 from necklaces_generation import *
 
@@ -14,3 +16,33 @@ def test_equal_necklaces_true():
 
 def test_equal_necklaces_false():
     assert not equal_necklaces([1, 0, 1, 1], [1, 0, 1, 0])
+
+
+@pytest.mark.parametrize(
+    "seq, seq_successor",
+    [
+        ([0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1]),
+        ([0, 0, 0, 0, 0, 1], [0, 0, 0, 0, 1, 0]),
+        ([0, 0, 0, 0, 1, 0], [0, 0, 0, 0, 1, 1]),
+        ([0, 0, 0, 0, 1, 1], [0, 0, 0, 1, 0, 0]),
+        ([0, 0, 0, 1, 0, 0], [0, 0, 0, 1, 0, 1]),
+        ([0, 0, 0, 1, 0, 1], [0, 0, 0, 1, 1, 0]),
+        ([0, 0, 0, 1, 1, 0], [0, 0, 0, 1, 1, 1]),
+        ([0, 0, 0, 1, 1, 1], [0, 0, 1, 0, 0, 1]),
+    ],
+)
+def test_successor(seq, seq_successor):
+    assert successor(seq) == seq_successor
+
+
+@pytest.mark.parametrize(
+    "sequence, index",
+    [
+        ([0, 1, 1, 0, 1], 3),
+        ([0, 1, 1, 1, 1], 0),
+        ([1, 1, 1, 1, 0], 4),
+        ([1, 1, 1, 1, 1], None),
+    ],
+)
+def test_find_largest_index_equal_to_zero(sequence, index):
+    assert find_largest_index_equal_to_zero(sequence) == index
