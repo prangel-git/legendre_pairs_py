a9902bb2800df40870da632508f831c67f853be4
prangel-git <62568827+prangel-git@users.noreply.github.com>
2022-01-31 23:44:03 -0500

    playing with bracelet generation.

--- necklaces_generation.py
+++ necklaces_generation.py
@@ -1,9 +1,10 @@
-from vector_utils import rotate_n
+from sequence_generation import seq_binary
+from vector_utils import rotate_n, reverse
 
 
 def find_necklace(sequence):
     potential_necklace = sequence.copy()
-    for k in range(len(sequence)):
+    for k in range(1, len(sequence)):
         rotated_sequence = rotate_n(sequence, k)
         if potential_necklace > rotated_sequence:
             potential_necklace = rotated_sequence
@@ -85,12 +86,72 @@ def repeat_tth_times_and_fill_jth_values(sequence, t, j):
     return sequence * t + sequence[:j]
 
 
+def find_bracelet(sequence):
+    potential_bracelet = sequence.copy()
+    for n in range(len(sequence)):
+        rotated_sequence = rotate_n(sequence, n)
+        if rotated_sequence < potential_bracelet:
+            potential_bracelet = rotated_sequence
+        if reverse(rotated_sequence) < potential_bracelet:
+            potential_bracelet = reverse(rotated_sequence)
+    return potential_bracelet
+
+
+def find_bracelet_from_necklace(sequence):
+    potential_bracelet = sequence.copy()
+    for n in range(len(sequence)):
+        reverse_rotated_sequence = reverse(rotate_n(sequence, n))
+        if reverse_rotated_sequence < potential_bracelet:
+            potential_bracelet = reverse_rotated_sequence
+    return potential_bracelet
+
+
+def seq_bracelets_of_half_density(length):
+    density = length // 2
+    current = [0] * (length - density) + [1] * density
+    last_necklace = [(i + 1) % 2 for i in range(length)]
+    last_necklace[0] = 0
+
+    yield current
+    while current < last_necklace:
+        i = find_largest_index_equal_to_zero(current) + 1
+        t = length // i
+        j = length % i
+
+        mod_current = current[: i - 1] + [1]
+        current = repeat_tth_times_and_fill_jth_values(mod_current, t, j)
+        if (
+            j == 0
+            and sum(current) == density
+            and current == find_bracelet_from_necklace(current)
+        ):
+            yield current
+    return
+
+
 def main():  # pragma: no cover
     print("Entry point for playing around")
     n = 7
     sequences = [seq for seq in fkm_algorithm([0] * n, [1] * n)]
+    count = 0
+
     for seq in sequences:
-        print(f"seq {seq} density {sum(seq)}")
+        print(
+            f"seq {seq} density {sum(seq)} count {count} is bracelet {seq == find_bracelet(seq)}"
+        )
+        count += 1
+
+    print(len(sequences))
+
+    count = 0
+    bracelet = set()
+    for seq in sequences:
+        seq_bracelet = tuple(find_bracelet(seq))
+        if seq_bracelet not in bracelet:
+            print(f"seq_bracelet {seq_bracelet} count {count}")
+            count += 1
+            bracelet.add(seq_bracelet)
+    print(count)
 
 
 if __name__ == "__main__":  # pragma: no cover
