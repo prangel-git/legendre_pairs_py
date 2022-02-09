e7e10ec092d268ab7f51a5b213d9c589d9811cf4
prangel-git <62568827+prangel-git@users.noreply.github.com>
2022-02-09 09:22:30 -0500

    Adding utility function for finding the maximum and its index.

--- other_utils.py
+++ other_utils.py
@@ -1,2 +1,6 @@
 def list_of_lists_to_set_of_tuples(list_of_lists):
     return set([tuple(l) for l in list_of_lists])
+
+
+def max_with_index(numbers):
+    return max(numbers), numbers.index(max(numbers))
--- test/test_other_utils.py
+++ test/test_other_utils.py
@@ -6,3 +6,8 @@ def test_list_of_lists_to_set_of_tuples():
     list_of_lists = [[1, 2, 3], [4, 5], [6]]
     set_of_tuples = set([(1, 2, 3), (4, 5), (6,)])
     assert list_of_lists_to_set_of_tuples(list_of_lists) == set_of_tuples
+
+
+def test_max_with_index():
+    numbers = [5, 2, 7, 1, 4, 7]
+    assert max_with_index(numbers) == (7, 2)
